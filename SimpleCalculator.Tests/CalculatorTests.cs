3523a560c69cd705466ff3a2b22f2485ae4eb93b
JZorvan <jzorvan@gmail.com>
2016-08-19 09:42:47 -0700

    Calculator Class handles all five 'rithmatics and tests pass on each

--- SimpleCalculator.Tests/CalculatorTests.cs
+++ SimpleCalculator.Tests/CalculatorTests.cs
@@ -11,11 +11,54 @@ namespace SimpleCalculator.Tests
     public class CalculatorTests
     {
         Calculator TestCalculator = new Calculator();
+
         [TestMethod]
         public void CanInstantiateAnInstanceOfCalculator()
         {
             Assert.IsNotNull(TestCalculator);
         }
 
+        [TestMethod]
+        public void CanAdd()
+        {
+            int First = 2, Second = 2;
+
+            Assert.AreEqual(TestCalculator.Addition(First, Second), 4);
+        }
+
+        [TestMethod]
+        public void CanSubtract()
+        {
+            int First = 3, Second = 2;
+
+            Assert.AreEqual(TestCalculator.Subtraction(First, Second), 1);
+
+        }
+
+        [TestMethod]
+        public void CanMultiply()
+        {
+            int First = 2, Second = 5;
+
+            Assert.AreEqual(TestCalculator.Multiplication(First, Second), 10);
+        }
+
+        [TestMethod]
+        public void CanDivide()
+        {
+            int First = 6, Second = 3;
+            int FirstFail = 3, SecondFail = 0;
+
+            Assert.AreEqual(TestCalculator.Division(First, Second), 2);
+            Assert.AreEqual(TestCalculator.Division(FirstFail, SecondFail), 0);
+        }
+
+        [TestMethod]
+        public void CanUseModulus()
+        {
+            int First = 6, Second = 4;
+
+            Assert.AreEqual(TestCalculator.Modulus(First, Second), 2);
+        }
     }
 }
--- SimpleCalculator/Calculator.cs
+++ SimpleCalculator/Calculator.cs
@@ -8,9 +8,33 @@ namespace SimpleCalculator
 {
     public class Calculator
     {
-        public void Calculate()
+        public int Addition(int First, int Second)
         {
-            
-        } 
+            return First + Second;
+        }
+        public int Subtraction(int First, int Second)
+        {
+            return First - Second;
+        }
+        public int Multiplication(int First, int Second)
+        {
+            return First * Second;
+        }
+        public int Division(int First, int Second)
+        {
+            if (Second == 0)
+            {
+                Console.WriteLine("You can't divide by zero, Holmes.");
+                return 0;
+            }
+            else
+            {
+                return First / Second;
+            }
+        }
+        public int Modulus(int First, int Second)
+        {
+            return First % Second;
+        }
     }
 }
